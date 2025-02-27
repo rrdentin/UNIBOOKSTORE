dac2203510ec157f62c4df74f6b5708eff07aeb5
rrdentin <denti.dona@gmail.com>
2025-02-27 20:16:39 +0700

    update

--- app/Filament/Pages/Pengadaan.php
+++ app/Filament/Pages/Pengadaan.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Pages;
 
+use App\Filament\Widgets\PengadaanStokWidget;
 use Filament\Pages\Page;
 use App\Filament\Widgets\PengadaanWidget;
 
@@ -12,16 +13,16 @@ class Pengadaan extends Page
     protected static string $view = 'filament.pages.pengadaan';
 
 
-    protected static ?string $navigationLabel = 'Kebutuhan Buku'; // Navigation label
-    protected static ?string $title = 'Pengadaan'; // Page title
-
+    protected static ?string $navigationLabel = 'Kebutuhan Buku';
+    protected static ?string $title = 'Pengadaan';
     protected static ?string $slug = 'kebutuhan-buku';
     protected static ?string $navigationGroup = 'Admin Management';
 
     protected function getHeaderWidgets(): array
     {
         return [
-                PengadaanWidget::class,
+            PengadaanStokWidget::class,    
+            PengadaanWidget::class,
         ];
     }
 
---
+++ app/Filament/Widgets/PengadaanStokWidget.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Buku;
+use App\Models\Penerbit;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class PengadaanStokWidget extends BaseWidget
+{
+    protected static ?string $maxHeight = null; 
+    protected int | string | array $columnSpan = 'full'; 
+    
+    public function table(Table $table): Table
+    {
+        $stokTerendah = Buku::min('stok');
+
+        return $table
+            ->query(
+                Buku::where('stok', $stokTerendah)
+                )
+            ->defaultPaginationPageOption(10)
+            ->columns([
+                TextColumn::make('nomor')->label('No.')
+                    ->rowIndex()
+                    ->sortable(false)
+                    ->wrap()
+                    ->grow(false)
+                    ->color(fn ($record) => $record->stok == $stokTerendah ? 'danger' : 'gray'),
+
+                TextColumn::make('nama_buku')->label('Nama Buku')
+                    ->sortable()
+                    ->wrap()
+                    ->grow(false)
+                    ->toggleable(false)
+                    ->searchable(true)
+                    ->color(fn ($record) => $record->stok == $stokTerendah ? 'danger' : 'gray'),
+
+                TextColumn::make('penerbit.nama')->label('Nama Penerbit')
+                    ->sortable()
+                    ->wrap()
+                    ->grow(false)
+                    ->toggleable(false)
+                    ->searchable(true)
+                    ->color(fn ($record) => $record->stok == $stokTerendah ? 'danger' : 'gray'),
+
+                TextColumn::make('stok')->label('Stok Tersisa')
+                    ->sortable()
+                    ->wrap()
+                    ->toggleable(false)
+                    ->grow(false)
+                    ->alignRight()
+                    ->searchable(true)
+                    ->color(fn ($record) => $record->stok == $stokTerendah ? 'danger' : 'gray'),
+            ])
+            
+            ->filters([
+                SelectFilter::make('id_penerbit')
+                ->label('Nama Penerbit')
+
+                    ->searchable()
+                    ->options(Penerbit::all()->pluck('nama', 'id'))
+                    ->multiple()
+            ]);
+    }
+}
